8ba7b0572a67ea8a949cf16d2c155365ca8326bb
twiest <twiest@0f671161-1e36-0410-9abd-759f811b91dd>
2008-02-26 07:24:53 +0000

    * gtkbook.com section's initial check-in * Chapter 02 check-in

---
+++ gtk-sharp/gtkbook.com/chapter_02/example_01-xtra_1-in_cs/example_01-xtra_1.cs
@@ -0,0 +1,73 @@
+//--------------------------------------------------------------------------------
+//
+// Copyright (C) 2008 Thomas Wiest
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------
+//
+// Converted from the examples in the book:
+//   Foundations of GTK+ Development
+//
+// Downloaded from:
+//   http://gtkbook.com/
+//
+//--------------------------------------------------------------------------------
+
+using System;
+using Gtk;
+
+namespace example_01
+{	
+	public class example_01
+	{		
+		public static void Main(string[] args) 
+		{			
+			//Application.Init();
+			if (Application.InitCheck("c02_e01b",ref args))			
+			{
+				Console.WriteLine("In GUI");
+			}
+			else
+			{
+				Console.WriteLine("In Console");
+				return;
+			}
+					
+			Window win = new Window(WindowType.Toplevel);
+			win.Title = "C02_E01-xtra_1 - in_cs - Hello World";
+			win.Show();
+			Application.Run();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

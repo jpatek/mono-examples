60238e066be4c012bd663da5d944af62b633a2e0
twiest <twiest@0f671161-1e36-0410-9abd-759f811b91dd>
2008-02-26 07:24:53 +0000

    * gtkbook.com section's initial check-in * Chapter 02 check-in

---
+++ trunk/gtk-sharp/gtkbook.com/chapter_02/example_01-in_c/example_01.c
@@ -0,0 +1,48 @@
+//--------------------------------------------------------------------------------
+//
+// Copyright (C) 2007 Andrew Krause
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------
+//
+// Examples from the book:
+//   Foundations of GTK+ Development
+//
+// Downloaded from:
+//   http://gtkbook.com/
+//
+// Modifications by:
+//   Thomas Wiest//
+//--------------------------------------------------------------------------------
+
+
+#include <gtk/gtk.h>
+
+int main (int argc,
+          char *argv[])
+{
+  GtkWidget *window;
+
+  /* Initialize GTK+ and all of its supporting libraries. */
+  gtk_init (&argc, &argv);
+
+  /* Create a new window, give it a title and display it to the user. */
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (window), "C02_E01 - in_c - Hello World");
+  gtk_widget_show (window);
+
+  /* Hand control over to the main loop. */
+  gtk_main ();
+  return 0;
+}

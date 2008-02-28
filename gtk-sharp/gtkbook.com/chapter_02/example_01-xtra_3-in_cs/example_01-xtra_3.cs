dcd9bb2ac3e6efc49308e6b45e3f0848b149e7f4
twiest <twiest@0f671161-1e36-0410-9abd-759f811b91dd>
2008-02-28 05:53:00 +0000

    * example_01-xtra_3-in_cs/_README_,   example_01-xtra_3-in_cs/example_01-xtra_3.cs: * Added the example 1   xtra 3 source files

---
+++ gtk-sharp/gtkbook.com/chapter_02/example_01-xtra_3-in_cs/example_01-xtra_3.cs
@@ -0,0 +1,71 @@
+//--------------------------------------------------------------------------------
+//
+// Copyright (C) 2008 Thomas Wiest (tw-mono_examples_project@wiestfamily.org)
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------
+//
+// Converted from the examples in the book:
+//   Foundations of GTK+ Development
+//
+// Downloaded from:
+//   http://gtkbook.com/
+//
+//--------------------------------------------------------------------------------
+
+using System;
+using Gtk;
+
+namespace example_01_xtra_1
+{	
+	public class example_01_xtra_1
+	{		
+		public static void Main(string[] args) 
+		{	
+			Window win;
+			
+			/* Initialize GTK+ and all of its supporting libraries. */
+			Application.Init();
+					
+			/* Create a new window, give it a title and display it to the user. */
+			win = new Window(WindowType.Toplevel);
+			win.Title = "C02_E01-xtra_3 - in_cs - Hello World";
+			win.Decorated = false;
+			win.Show();
+			
+			/* Hand control over to the main loop. */
+			Application.Run();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

f05413c31f35af6aa5ff9bd57f181b3fb6098e6a
twiest <twiest@0f671161-1e36-0410-9abd-759f811b91dd>
2008-03-02 21:20:18 +0000

    * Added example_02-xtra_3 which shows how to set the default size of a window and how to move it around the screen.

---
+++ gtk-sharp/gtkbook.com/chapter_02/example_02-xtra_3-in_c/main.c
@@ -0,0 +1,88 @@
+//--------------------------------------------------------------------------------
+//
+// Copyright (C) 2007 Andrew Krause
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------
+//
+// Examples from the book:
+//   Foundations of GTK+ Development
+//
+// Downloaded from:
+//   http://gtkbook.com/
+//
+// Modifications by:
+//   Thomas Wiest (tw-mono_examples_project@wiestfamily.org)//
+//--------------------------------------------------------------------------------
+
+
+
+#include <gtk/gtk.h>
+
+static void destroy (GtkWidget*, gpointer);
+static gboolean delete_event (GtkWidget*, GdkEvent*, gpointer);
+
+int main (int argc, 
+          char *argv[])
+{
+  GtkWidget *window, *label;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (window), "C02_E02 - in_c - Hello World!");
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+  gtk_widget_set_size_request (window, 200, 100);
+  
+  // Set the default size of the window, notice the window can still be shrunk.
+  gtk_window_set_default_size (window, 400, 100);
+  
+  // Move the window away from the upper left corner of the screen.
+  gtk_window_move(window, 200, 200);
+
+  /* Connect the main window to the destroy and delete-event signals. */
+  g_signal_connect (G_OBJECT (window), "destroy",
+                    G_CALLBACK (destroy), NULL);
+  g_signal_connect (G_OBJECT (window), "delete_event",
+                    G_CALLBACK (delete_event), NULL);
+
+  /* Create a new GtkLabel widget that is selectable. */
+  label = gtk_label_new ("Hello World");
+  gtk_label_set_selectable (GTK_LABEL (label), TRUE);
+
+  /* Add the label as a child widget of the window. */
+  gtk_container_add (GTK_CONTAINER (window), label);
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+  return 0;
+}
+
+/* Stop the GTK+ main loop function. */
+static void
+destroy (GtkWidget *window,
+         gpointer data)
+{
+  gtk_main_quit ();
+}
+
+/* Return FALSE to destroy the widget. By returning TRUE, you can cancel
+ * a delete-event. This can be used to confirm quitting the application. */
+static gboolean
+delete_event (GtkWidget *window,
+              GdkEvent *event,
+              gpointer data)
+{
+  return FALSE;
+}

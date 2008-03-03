5c8e385aaf4e9786428449597879f12811d82388
twiest <twiest@0f671161-1e36-0410-9abd-759f811b91dd>
2008-03-03 02:41:38 +0000

    * exercise_2-in_cs, exercise_2-in_cs/exercise_2-in_cs.mdp,   exercise_2-in_cs/main.cs, exercise_2-in_c, exercise_2-in_c/main.c,   exercise_2-in_c/exercise_2-in_c.mdp: Added chapter 2 exercise 2

---
+++ trunk/gtk-sharp/gtkbook.com/chapter_02/exercise_2-in_c/main.c
@@ -0,0 +1,129 @@
+//--------------------------------------------------------------------------------
+//
+// Copyright (C) 2007 Andrew Krause
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------
+//
+// Examples from the book:
+//   Foundations of GTK+ Development
+//
+// Downloaded from:
+//   http://gtkbook.com/
+//
+// Modifications by:
+//   Thomas Wiest (tw-mono_examples_project@wiestfamily.org)//
+//--------------------------------------------------------------------------------
+
+
+
+#include <gtk/gtk.h>
+
+static void destroy (GtkWidget*, gpointer);
+static gboolean key_press_event (GtkWidget   *widget,
+							     GdkEventKey *event,
+							     gpointer     user_data);
+static void title_changed (GObject *widget,
+						   GParamSpec *property,
+						   gpointer data);
+
+int main (int argc, 
+          char *argv[])
+{
+  GtkWidget *window, *label;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  
+  // Use g_object_set instead of the gtk_ helper functions
+  //gtk_window_set_title (GTK_WINDOW (window), "Thomas");
+  g_object_set (window, "title", "Thomas", NULL);
+  
+  // Notify when the title changes
+  g_signal_connect (G_OBJECT (window), "notify::title",
+                    G_CALLBACK (title_changed), NULL);
+  
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+  
+  // Use g_object_set instead of the gtk_ helper functions
+  //gtk_widget_set_size_request (window, 300, 100);
+  g_object_set (window, "width-request", 300, NULL);
+  g_object_set (window, "height-request", 100, NULL);
+  
+  // Make the window not resizeable
+  gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
+
+  /* Connect the main window to the destroy and delete-event signals. */
+  g_signal_connect (G_OBJECT (window), "destroy",
+                    G_CALLBACK (destroy), NULL);
+                    
+  g_signal_connect (G_OBJECT (window), "key-press-event",
+  					G_CALLBACK (key_press_event), NULL);
+
+  /* Create a new GtkLabel widget that is selectable. */
+  label = gtk_label_new ("Wiest");
+  gtk_label_set_selectable (GTK_LABEL (label), TRUE);
+
+  /* Add the label as a child widget of the window. */
+  gtk_container_add (GTK_CONTAINER (window), label);
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+  return 0;
+}
+
+/* Stop the GTK+ main loop function. */
+static void
+destroy (GtkWidget *window,
+         gpointer data)
+{
+  gtk_main_quit ();
+}
+
+// Swap the title and the label text
+static gboolean
+key_press_event (GtkWidget   *window,
+			   GdkEventKey *event,
+			   gpointer     user_data)
+{
+	GtkWidget *label = gtk_bin_get_child (GTK_BIN (window));
+	const gchar *tmp_title = gtk_window_get_title (GTK_WINDOW (window));
+	
+	if (g_ascii_strcasecmp(tmp_title, "Thomas") == 0)
+	{
+		// Use g_object_set instead of the gtk_ helper functions
+		//gtk_window_set_title (GTK_WINDOW (window), "Wiest");
+		g_object_set (window, "title", "Wiest", NULL);
+		
+		//gtk_label_set_text (GTK_LABEL (label), "Thomas");
+		g_object_set (label, "label", "Thomas", NULL);	
+	}
+	else
+	{
+		// Use g_object_set instead of the gtk_ helper functions
+		//gtk_window_set_title (GTK_WINDOW (window), "Thomas");
+		g_object_set (window, "title", "Thomas", NULL);
+  
+		//gtk_label_set_text (GTK_LABEL (label), "Wiest");
+		g_object_set (label, "label", "Wiest", NULL);
+	}
+}
+
+static void title_changed (GObject *widget,
+						   GParamSpec *property,
+						   gpointer data)
+{
+	g_message("title changed");
+}
